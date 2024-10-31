86d6bafabdcb0beefbaa869e8558be512230a5ef
dandikamarthachristiyo <dandika.martha@gmail.com>
2024-10-31 18:48:27 +0700

    pertanyaan 3

--- ArrayRataNillai07.java
+++ ArrayRataNillai07.java
@@ -4,18 +4,30 @@ public class ArrayRataNillai07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] nilaiMhs = new int[10];
         double total = 0;
-        double rata2;
+        double ratalulus, rataTidakLulus;
+        int nilaiLulus = 0, lulus = 0;
+        int nilaiTidakLulus = 0, tidakLulus = 0;
+        int n;
+        
+        System.out.print("Masukkan jumlah Mahasiswa = ");
+        n = sc.nextInt();
 
+        int [] nilaiMhs = new int[n];
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai Mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+                nilaiLulus += nilaiMhs[i];
+            } else if (nilaiMhs[i] <= 70) {
+                tidakLulus++;
+                nilaiTidakLulus += nilaiMhs[i];
+            }
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        ratalulus = nilaiLulus / lulus;
+        rataTidakLulus = nilaiTidakLulus / tidakLulus;
+        System.out.println("Rata-rata nilai lulus = "+ratalulus);
+        System.out.println("Banyak Mahasiswa tidak lulus = "+rataTidakLulus);
     }
 }
